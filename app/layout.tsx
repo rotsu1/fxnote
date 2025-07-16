7a16bcf9fef435ce0d658ec26519850317cf1535
rotsu1 <ryuotsu123@gmail.com>
2025-07-16 16:56:40 +0900

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
