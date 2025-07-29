734e15de3f1d2cfd0ec0bb0cf9f2b09acdba7e32
rotsu1 <ryuotsu123@gmail.com>
2025-07-29 20:26:41 +1000

    Added seconds in entry time and exit time in trade records displayed in the right side bar

--- app/dashboard/calendar/page.tsx
+++ app/dashboard/calendar/page.tsx
@@ -420,7 +420,8 @@ function TradeCard({
                   month: '2-digit',
                   day: '2-digit',
                   hour: '2-digit',
-                  minute: '2-digit'
+                  minute: '2-digit',
+                  second: '2-digit'
                 }) : trade.time}
               </div>
             )}
@@ -431,7 +432,8 @@ function TradeCard({
                   month: '2-digit',
                   day: '2-digit',
                   hour: '2-digit',
-                  minute: '2-digit'
+                  minute: '2-digit',
+                  second: '2-digit'
                 }) : ""}
               </div>
             )}
