0a473c46dfda8f7083fa33a1f81548c9404968a9
rotsu1 <ryuotsu123@gmail.com>
2025-08-03 16:52:41 +1000

    Fixed small bug

--- app/dashboard/table/page.tsx
+++ app/dashboard/table/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useMemo, useCallback, useEffect } from "react"
+import React, { useState, useMemo, useCallback, useEffect } from "react"
 
 import {
   CalendarIcon,
